390ff83012060e4ee9839afb007c9926c28b11cc
Siddhesh Jadhav <siddhesh0@Siddhesh.Jadhav>
2026-06-30 00:15:12 +0530

    sunderstand the concepts

---
+++ basic/a_b.py
@@ -0,0 +1,6 @@
+a =  int(input("Enter a number: "))
+
+b= int(input(" enter a number : "))
+
+c = a + b
+print("The sum is:", c)
---
+++ basic/text.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("Hello Siddhesh")
+
+engine.runAndWait()
+
+print("Done")
