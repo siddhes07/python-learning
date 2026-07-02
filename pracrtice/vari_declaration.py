c9d3a130446c4eb3d35d59c87b136421bfa99cf8
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-07-03 01:22:16 +0530

    solve the basic variable problem of print name age and height in python

---
+++ pracrtice/vari_declaration.py
@@ -0,0 +1,8 @@
+name = input(" enter youe name ")
+age= int ( input ( " enter your age "))
+height = float ( input ( " enter your height "))
+
+print(f" your name is : {name} ")
+print(f" your age is : {age} ")
+print(f" your height is : {height} ")
+
