d514889d10aa3f15ef30d36c02f5ba9f1d5f7854
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-07-06 19:46:08 +0530

    solve print result problem useing python statements

---
+++ pracrtice/result.py
@@ -0,0 +1,15 @@
+marks = int (input ( " enter your marks : " ) )
+
+if ( marks >= 90):
+    print ( "grade = 'a'" )
+
+elif ( marks >=80):
+    print ( "grade = 'b'" )
+
+elif (marks>=70):
+    print ( "grade = 'c'" )
+
+elif (marks>=60):
+    print ( "grade = 'd'" )
+else:
+    print ( " fail" )
