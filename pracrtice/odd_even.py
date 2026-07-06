1e777e07d1349431ebd5ec3fe89265a59a91f6f8
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-07-06 20:42:10 +0530

    cheak num is even or odd useing condition statements

---
+++ pracrtice/odd_even.py
@@ -0,0 +1,9 @@
+number = int ( input ( " enter a number : "))
+
+if ( number % 2 ==0 ) :
+    print ( " even number " )
+elif ( number % 3 !=0 ) :
+    print ( " odd number " )
+
+elif ( number == 0 ) :
+    print ( " zero "    )
