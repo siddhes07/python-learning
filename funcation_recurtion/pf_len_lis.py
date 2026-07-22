970d3b5d337a91d49129d252a847c9e67237e477
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-07-22 23:42:08 +0530

    print the lenth of the parameters in strings

---
+++ funcation_recurtion/pf_len_lis.py
@@ -0,0 +1,8 @@
+cities = [ "pune", "mumbai", "delhi", "bangalore", "chennai" ]
+heros = [ "ironman", "spiderman", "batman", "superman", "hulk" ]
+
+def print_len(list):
+    print(len(list))
+
+print_len(heros)
+print_len(cities)
