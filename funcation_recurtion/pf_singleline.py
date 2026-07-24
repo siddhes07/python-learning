ec8f7c138ee294f3f085e8562c9596b57a851739
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-07-24 22:55:06 +0530

    print char in one line

---
+++ funcation_recurtion/pf_singleline.py
@@ -0,0 +1,12 @@
+boys = [ " siddhesh", "sachin", "rohit", "virat", "dhoni" ]
+cities = [ "pune", "mumbai", "delhi", "bangalore", "chennai" ]
+
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item , end = " ")
+
+print_list(boys)
+print()
