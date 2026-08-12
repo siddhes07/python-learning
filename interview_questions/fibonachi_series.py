c95706d79c834db1b83b5952ed7804458b20900a
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-08-12 23:29:07 +0530

    solve fibonachi series problem

---
+++ interview_questions/fibonachi_series.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    a, b = 0, 1
+    series = []
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+n = int(input("Enter number of terms: "))
+print(fibonacci(n))
