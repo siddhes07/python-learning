7c481d8b2149d2c0be4da943ed95f7a4826dc25e
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-08-14 23:03:04 +0530

    find missing numbers

---
+++ interview_questions/find_missing.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 5, 6]
+
+n = len(numbers) + 1
+
+expected_sum = n * (n + 1) // 2
+
+actual_sum = 0
+
+for num in numbers:
+    actual_sum += num
+
+missing = expected_sum - actual_sum
+
+print("Missing number:", missing)
