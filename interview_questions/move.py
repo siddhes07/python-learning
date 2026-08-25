efa2426cd8091c107ce4d47488adc56e7fe723d4
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-08-25 23:21:36 +0530

    remove all zero in last

---
+++ interview_questions/move.py
@@ -0,0 +1,18 @@
+numbers = input("enter numbers : ").split()
+
+
+
+result = []
+zero_count = 0
+
+for num in numbers:
+
+    if num == 0:
+        zero_count += 1
+    else:
+        result.append(num)
+
+for i in range(zero_count):
+    result.append(0)
+
+print(result)
