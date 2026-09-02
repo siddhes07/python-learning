34374f97079960f32674d8306bb2197a37d76eb9
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-09-02 23:38:44 +0530

    solve problem

---
+++ pracrtice/sudoco.py
@@ -0,0 +1,68 @@
+def is_valid(board, row, col, num):
+    # Check row
+    if num in board[row]:
+        return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check 3x3 box
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+
+    return None
+
+
+def solve(board):
+    empty = find_empty(board)
+
+    if empty is None:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve(board):
+    for row in board:
+        print(row)
+else:
+    print("No solution exists.")
