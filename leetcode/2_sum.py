a076b39c8dc4be33d2a56d17f0e1db94b5a73d9c
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-09-04 23:09:29 +0530

    add leetcode folder and solve 2 sum problem

---
+++ leetcode/2_sum.py
@@ -0,0 +1,13 @@
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+target = int(input("Enter target: "))
+
+seen = {}
+
+for i, num in enumerate(nums):
+    complement = target - num
+
+    if complement in seen:
+        print([seen[complement], i])
+        break
+
+    seen[num] = i
