d5a412930558f46ef95fc4f7503ba51391a49346
Siddhesh Jadhav <siddheshnj07@gmail.com>
2026-09-05 19:27:50 +0530

    large num

---
+++ if_and_else/larg_three.py
@@ -0,0 +1,10 @@
+a = int(input(" enter a first number "))
+b = int(input(" enter a second number "))
+c = int(input(" enter a third number "))
+
+if a > b and a >c:
+    print(" a is greater ")
+elif b > a and b > c:
+    print(" b is greater ")
+else:
+    print(" c is greater ") 
