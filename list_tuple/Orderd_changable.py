5b4c1b38a42214b60eda0b527c40879fe2bf675d
siddhes07 <siddheshnj07@gmail.com>
2026-09-26 22:47:01 +0530

    Add example of list operations in Orderd_changable.py
    Demonstrate list operations including creation, access, addition, modification, removal, and length calculation.

---
+++ list_tuple/Orderd_changable.py
@@ -0,0 +1,23 @@
+# Creating a list
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print(fruits)
+
+# Accessing elements
+print(fruits[0])
+print(fruits[2])
+
+# Adding an element
+fruits.append("Grapes")
+print(fruits)
+
+# Changing an element
+fruits[1] = "Pineapple"
+print(fruits)
+
+# Removing an element
+fruits.remove("Mango")
+print(fruits)
+
+# Length of list
+print(len(fruits))
